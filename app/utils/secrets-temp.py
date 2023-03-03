61d934b756633f80b7638b05b5094a3afa922dde
dylanp1212 <dylan.paltiel@gmail.com>
2023-03-03 10:10:28 -0800

    mongodb set up

--- app/utils/secrets-temp.py
+++ app/utils/secrets-temp.py
@@ -2,8 +2,8 @@ import os
 
 def getSecrets():
     secrets = {
-        'MONGO_HOST':'',
-        'MONGO_DB_NAME':'',
+        'MONGO_HOST':'mongodb+srv://dylanpaltiel:<dpmongodb>@cluster0.k1ulez0.mongodb.net/CapstoneDB?retryWrites=true&w=majority',
+        'MONGO_DB_NAME':'CapstoneDB',
         'GOOGLE_CLIENT_ID': '',
         'GOOGLE_CLIENT_SECRET':'',
         'GOOGLE_DISCOVERY_URL':"https://accounts.google.com/.well-known/openid-configuration",
