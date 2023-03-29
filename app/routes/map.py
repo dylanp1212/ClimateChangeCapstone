c6526d13b9a7975c43fc7be301ed5b3ad27ce4a9
dylanp1212 <dylan.paltiel@gmail.com>
2023-03-29 10:15:15 -0700

    map map

---
+++ app/routes/map.py
@@ -0,0 +1,17 @@
+from app import app
+from app.utils.secrets import getSecrets
+import requests
+from flask import render_template, flash, redirect, url_for
+import requests
+from flask_login import current_user
+from app.classes.data import Clinic
+from app.classes.forms import ClinicForm
+#from flask_login import login_required
+import datetime as dt
+
+@app.route('/map/map')
+def clinicMap():
+
+    clinics = Clinic.objects()
+
+    return render_template('cliniclocator.html',clinics=clinics)
