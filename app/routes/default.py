6314930e3110e49e94ad8b54fee810188e0a0779
dylanp1212 <dylan.paltiel@gmail.com>
2023-04-10 04:20:20 -0700

    figuring out map

--- app/routes/default.py
+++ app/routes/default.py
@@ -9,3 +9,19 @@ def index():
 @app.route('/aboutus')
 def aboutus():
     return render_template('aboutus.html')
+
+@app.route('/news')
+def news():
+    return render_template('news.html')
+
+@app.route('/contacts')
+def contacts():
+    return render_template('contacts.html')
+
+@app.route('/unionizationwhy')
+def unionizationwhy():
+    return render_template('unionizationwhy.html')
+
+@app.route('/unionizationhow')
+def unionizationhow():
+    return render_template('unionizationhow.html')
