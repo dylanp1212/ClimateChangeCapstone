220a16351dcbab013fe0d3b5944bfc5afa8e5297
dylanp1212 <dylan.paltiel@gmail.com>
2023-04-14 09:29:50 -0700

    role into user

--- app/classes/data.py
+++ app/classes/data.py
@@ -31,6 +31,7 @@ class User(UserMixin, Document):
     email = EmailField()
     image = FileField()
     prononuns = StringField()
+    role = StringField()
 
     meta = {
         'ordering': ['lname','fname']
